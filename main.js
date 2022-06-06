615e7db957628979e897204c7c713004de08f28a
Lucy de Rojas <lucyderojas@gmail.com>
2022-06-06 08:13:07 +0100

    drafted

--- main.js
+++ main.js
@@ -1,5 +1,47 @@
 /* 
 👉 Write your kata's scenario/description here!
+
+
+
+
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+function confuseMe(firstName, lastName) {
+
+
+    let person = {
+        name: firstName,
+        surname: lastName, 
+
+
+        shuffle: ()=>{
+
+            console.log('shuffling');
+
+    
+            person.name = lastName;
+            person.surname = firstName;
+
+            return person;
+
+            
+        }
+
+
+
+
+    };
+
+
+   
+    return person;
+
+
+}
+
+
+
+
+let lucy = confuseMe('lucy', 'rojas').shuffle();
+console.log('finished: >>>>> ',lucy);
